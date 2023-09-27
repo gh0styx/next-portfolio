db3368d5c099c3454472f0f74f935316273d0e4f
1lexaa <nice140@icloud.com>
2023-09-27 16:17:14 +0300

    !!!

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -4,25 +4,30 @@ import { AiFillGithub } from "react-icons/ai";
 const ProjectCard = (props: any) => {
   return (
     <>
-      <div className="m-5 w-1/3 block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-black">
+      <div className=" m-5 lg:w-1/3  lg:block   rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-black">
         <a href="#!">
-          <img className="rounded-t-lg" src="/react.png" alt="" />
+          <img className="rounded-t-lg  h-[250px]" src={props.img} alt="" />
         </a>
-        <div className="p-6">
+        <div className="p-10">
           <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
             {props.title}
           </h5>
           <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
             {props.text}
           </p>
-          <a href={props.url}>
-            <button
-              type="button"
-              className="inline-block rounded bg-primary  text-xl font-medium uppercase leading-normal text-black dark:text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-            >
-              <AiFillGithub />
-            </button>
-          </a>
+          <div className="inline-block p-2">
+            <a href={props.url}>
+              <button
+                type="button"
+                className=" rounded  p-2  font-medium uppercase text-sm leading-normal text-black dark:text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+              >
+                <AiFillGithub size={20} />
+              </button>
+            </a>
+            {/* <button className=" bg-red-400 m-2 rounded mb-1.5 p-2  text-sm font-medium uppercase leading-normal text-black dark:text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+              Live Server
+            </button> */}
+          </div>
         </div>
       </div>
     </>
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -7,106 +7,27 @@ import ProjectCard from "@/components/ProjectCard";
 const projects = () => {
   return (
     <>
-      <main className="px-10 bg-white dark:bg-black">
+      <main className="px-10 bg-white dark:bg-black ">
         <section className="">
           <Nav />
           <div className="text-center p-10 items-center text-3xl ">
-            <h1>My Projects</h1>
+            <h1 className="font-mono animate-bounce">My Projects</h1>
           </div>
 
           <div className="flex justify-center flex-wrap">
             <ProjectCard
-              title="Tech-Shop"
-              text="Next.js Tech-Shop. MongoDB. E-commerce store"
+              img="/tech-shop.png"
+              title="Shop"
+              text="Next.js Tech-Shop. MongoDB. Store"
               url="https://github.com/1lexaa/next-tech-shop"
             />
             <ProjectCard
-              title="Card Title"
-              text=" Some quick example text to build on the card title and make up
-                  the bulk of the card's content."
+              img="/quiz.png"
+              title="Quiz"
+              text="React.js Quiz App. MongoDB. Quiz"
+              url="https://github.com/1lexaa/Quiz"
             />
-            <ProjectCard
-              title="Card Title"
-              text=" Some quick example text to build on the card title and make up
-                  the bulk of the card's content."
-            />
-            <ProjectCard
-              title="Card Title"
-              text=" Some quick example text to build on the card title and make up
-                  the bulk of the card's content."
-            />
-
-            {/* <div className="m-5 w-1/3 block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-black">
-              <a href="#!">
-                <img className="rounded-t-lg" src="/react.png" alt="" />
-              </a>
-              <div className="p-6">
-                <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-                  Card title
-                </h5>
-                <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <button
-                  type="button"
-                  className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-black dark:text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                >
-                  More
-                </button>
-              </div>
-            </div>
-            <div className="m-5 w-1/3 block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-black">
-              <a href="#!">
-                <img className="rounded-t-lg" src="/react.png" alt="" />
-              </a>
-              <div className="p-6">
-                <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-                  Card title
-                </h5>
-                <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <button
-                  type="button"
-                  className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                >
-                  More
-                </button>
-              </div> */}
-
-            {/* <div className="border border-black dark:border-white rounded-lg w-2/5 items-center m-5">
-            <img src="/react.png" alt="1st project" className="p-3 " />
-            <div className="p-3">
-              <h2 className="text-center text-xl font-medium">Project Name</h2>
-              <div className="flex items-center">
-                <div className="mt-3 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                  Read more
-                  <svg
-                    className="w-3.5 h-3.5 ml-2"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </div> */}
           </div>
-
-          {/* <Footer /> */}
         </section>
       </main>
     </>
